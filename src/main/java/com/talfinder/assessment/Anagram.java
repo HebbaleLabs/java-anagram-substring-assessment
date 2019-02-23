9ed8288b03773114bb209bdefec54da5171e8f08
Shashidhar G A <shashidhar.a@hebbalelabs.com>
2019-02-23 18:18:57 +0530

    Initial commit

---
+++ src/main/java/com/talfinder/assessment/Anagram.java
@@ -0,0 +1,21 @@
+package com.talfinder.assessment;
+
+public class Anagram {
+
+  public static int countChanges(String input) {
+    /*
+     * NOTE:
+     * 1. Write your solution within this method
+     * 2. Do not make changes to the Project, Class Name, Method Name or Method Signature
+     * 3. Use the Run-Java command to run main method
+     * 4. Use the BuildAndRunTests command to evaluate your solution
+     */
+    int changesCount = 0;
+    return changesCount;
+  }
+
+  public static void main(String[] args) {
+    String input = "abcb";
+    System.out.println(countChanges(input));
+  }
+}
